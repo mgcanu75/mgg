5d499101a223c80f22d5cf8f9ff6bd20c534de79
garluc <mgcanu75@gmail.com>
2020-02-24 21:02:29 +0100

    añadida documentacion Miguel Garcia Garre

--- ejemplo.php
+++ ejemplo.php
@@ -1,5 +1,25 @@
 <?php
 
+/**
+* Ejemplo.php
+* En esta clase se dfine el metodo sumarDosNumeros y una clase llamada Ejemplo.
+*
+*/
+
+/**
+* Sumar dos numeros
+*
+* Funcion que permite calcular la suma de dos numeros.
+*
+
+*/
+
+/**
+* Funcion que permite calcular la multiplicacion de tres numeros
+* 
+* @autor Miguel
+*/
+
 function multiplicarTresNumeros ($v1, $v2, $v3)
 {
 	return $v1*$v2*$v3;
@@ -17,3 +37,4 @@ class Ejemplo {
 	}
 
 }
+
